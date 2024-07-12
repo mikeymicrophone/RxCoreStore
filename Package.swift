4acc6cffb8a055d4de416d14098f90dd12c80c1d
Mike Schwab <mike.schwab@gmail.com>
2024-07-12 12:51:51 -0400

    swift-tools-version syntax and update

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 //
-//swift-tools-version:5.3
+// swift-tools-version: 5.10
 
 import PackageDescription
 
